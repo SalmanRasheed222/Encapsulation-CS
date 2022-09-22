8a7ff411234bb6745704835e8f112b8da1885105
SALMAN <SALMAN@DESKTOP-Q0BOMET>
2022-09-22 23:33:48 +0500

    Add project files.

---
+++ Encapsulation CS/Program.cs
@@ -0,0 +1,16 @@
+﻿using Encapsulation_CS;
+
+
+
+
+
+student s1 = new student();
+//student s2 = new student();
+//s1.totalmarks = 1100;
+Console.Write("enter name: ");
+s1.name = Console.ReadLine();
+Console.WriteLine("enter marks you got:");
+s1.marks = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("percentage="+s1.percentage);
+
+
---
+++ Encapsulation CS/Student.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation_CS
+{
+	 
+	public class student
+	{
+		public string name;
+		public int marks;
+	   private int totalmarks=1100;
+
+
+		public decimal percentage
+		{
+			get
+			{
+				return Convert.ToDecimal(marks) / Convert.ToDecimal(totalmarks) * 100;
+			}
+		}
+	
+
+	}
+
+	
+
+
+
+
+
+
+
+}
